b9f21bf937137e2885862d0fa3650985c49df98b
JuniorN1 <junior_e.a.o@hotmail.com>
2021-03-02 17:27:04 -0300

    config routes.js add route chat and route enter

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,16 @@ import React from 'react';
 import {NavigationContainer}  from '@react-navigation/native'
 import{createStackNavigator} from '@react-navigation/stack';
 const {Navigator,Screen} = createStackNavigator();
-import Enter from './enter'
+import Enter from './enter';
+import Chat from './chat';
 export default function App() {
     return (
         <NavigationContainer >
             <StatusBar hidden={false}/>
             <Navigator screenOptions={{headerShown:false}}>
-                <Screen component={login} name="login"/>
-                </Navigator>
-                </NavigationContainer>
+                <Screen component={Enter} name="Enter"/>
+                <Screen component={Chat} name="Chat"/>
+            </Navigator>
+        </NavigationContainer>
     )
 }
