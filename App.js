1859b5f4e6271f33f01a12531d7d8499e4e53676
JuniorN1 <junior_e.a.o@hotmail.com>
2021-03-02 15:38:39 -0300

    yarn add parse to back4app to include class save in send

--- App.js
+++ App.js
@@ -2,27 +2,30 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { Image, Linking, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat'
-
+import { AsyncStorage } from 'react-native';
+import Parse from 'parse/react-native.js';
 export default function App() {
   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ])
-  }, [])
-
+ 
+  Parse.setAsyncStorage(AsyncStorage);
+  Parse.initialize("ZdeOc2OKVbRCLb8wiUHEa6oWxRCjrf3pY89xLuEm","Fe1Bn8OxU2Q8Az0EuzHPctZfK0i3KgXnkNDHSxuW"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+Parse.serverURL = 'https://parseapi.back4app.com/';
   const onSend = useCallback((messages = []) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
+    console.log(messages);
+    // const MyFirstClass = Parse.Object.extend("MyFirstClass");
+    // const myFirstClass = new MyFirstClass();
+
+    // myFirstClass.set("name", "I'm able to save objects!");
+    // myFirstClass.save()
+    // .then((object) => {
+    //   // Success
+    //   console.log('New object created with objectId: ' + object.id);
+    // }, (error) => {
+    //   // Save fails
+    //   alert('Failed to create new object, with error code: ' + error.message);
+    // });
   }, [])
 
   return (
